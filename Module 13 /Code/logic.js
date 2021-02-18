3d01405c3fd8c06ec331dba9d3dbde5060c86901
arahogc <74072742+arahogc@users.noreply.github.com>
2021-02-18 09:15:59 -0800

    Add files via upload
    Copies of code to date as well as a screenshot of the console

---
+++ Module 13 /Code/logic.js
@@ -0,0 +1,26 @@
+//Add console.log to check to see if our code is working 
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [
+      40.7, -94.5
+    ],
+    zoom: 4
+  })
+  
+console.log("chek");
+
+//Add tile layer that will be the background of the map
+//Note: had to use code from John in Slack, not from the modules 
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,\
+    <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+})
+
+console.log("streets");
+
+//Then we add our 'graymap' tile layer to the map 
+streets.addTo(map); 
